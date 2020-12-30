ae3cbf7ab9cfbdf6282da1d0abefc002ac36f855
Jack Keane <john.f.keane.22@dartmouth.edu>
2020-12-30 00:59:18 -0700

    add acronym formatting

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsComponent implements OnInit {
 
+  topAcro = "testing exact silly things";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getAcro(acronym: string): string {
+    return acronym.split(' ').map((word) => {
+      return `<b>${word[0].toLocaleUpperCase()}</b>${word.slice(1)}`
+    }).join('<br>');
   }
 
 }
